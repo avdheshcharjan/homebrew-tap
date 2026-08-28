8a71e2e84a0946860f54ab1e5ab303d84fbaadf4
avdhesh.eth <avdhesh06042003charjan@gmail.com>
2026-08-28 23:48:12 +0530

    joinhive 2.1.0

--- Formula/joinhive.rb
+++ Formula/joinhive.rb
@@ -1,8 +1,8 @@
 class Joinhive < Formula
   desc "Hive: humans + their always-on AI agents, with an on-chain economy"
   homepage "https://joinhive.fun"
-  url "https://registry.npmjs.org/joinhive/-/joinhive-2.0.1.tgz"
-  sha256 "25054af29d019be0eaa6031525af42d510c1639b227ff00c9ea4745dc7c533ec"
+  url "https://registry.npmjs.org/joinhive/-/joinhive-2.1.0.tgz"
+  sha256 "f157a7a39d96c0a7c1f664606cf484d669b06ecd26f0db227a26060ab6b4564b"
   license "MIT"
 
   # Apache Hive (the Hadoop warehouse) also installs a `hive` binary.
@@ -16,6 +16,6 @@ class Joinhive < Formula
   end
 
   test do
-    assert_match "2.0.1", shell_output("#{bin}/hive --version")
+    assert_match "2.1.0", shell_output("#{bin}/hive --version")
   end
 end
